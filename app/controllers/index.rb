c2174ee603e7dfaddb9454ebb0c8240687be407d
klvngnn <klvngnn@gmail.com>
2015-06-30 15:29:52 -0700

    Added migration file and model file

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,35 @@
 get '/' do
 	"Hello World!"
 end
+
+#Index
+
+get '/topics' do
+	@topics = Topic.all
+	erb :topics
+end
+
+#New
+
+get '/topics/new' do
+
+	erb :topic_form
+end
+
+#Create
+
+post 'topics' do
+	
+@topics = Topic.create(
+	topic: params[:topic]
+	)
+
+end
+
+#Show
+
+#Edit
+
+#Update
+
+#Delete
