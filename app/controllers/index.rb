d9d2a8bdd30cd9cc9ff9df1be317e33eb0c634a6
Mark McQuillen <markdmcq@hotmail.com>
2015-06-30 16:21:18 -0700

    Build topics list with topic page with comments

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,17 @@
 get '/' do
-	"Hello World!"
+	@topics = Topic.all
+	erb :index
 end
+
+post '/topics' do
+	p params[:topic_title]
+	Topic.create(title: params[:topic_title])
+	redirect '/'
+end
+
+
+get '/topics/:id' do
+	@topic = Topic.find(params[:id])
+	erb :show
+end
+
